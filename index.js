73b531bf631df3c1cbb5e8569ce5c86aad96091a
Ilhammeru <gumilang.dev@gmail.com>
2022-02-24 04:28:13 +0700

    change

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const product = require("./api/product");
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/product", product);
+app.use("/api/product", (req, res) => {
+    res.json({message: "Product"});
+});
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 
-module.exports = app;
+// module.exports = app;
